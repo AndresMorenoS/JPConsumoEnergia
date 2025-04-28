517519c5ba754241acd4bf27722157354a949155
AndresMorenoS <afmoreno22@outlook.com>
2025-04-27 20:51:32 -0500

    Adding essencial to models

---
+++ src/energia/models/ClienteNoRegulado.java
@@ -0,0 +1,9 @@
+package energia.models;
+
+public class ClienteNoRegulado {
+    private String idUnico;
+    private String tipoIdentificacion;
+    private String correo;
+    private String direccion;
+
+}
